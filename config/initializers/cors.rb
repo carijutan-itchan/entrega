efda539ea827ff1390b90ca4d336f7a449c14af5
itchan <christianbaman4@gmail.com>
2023-06-11 23:30:57 +0800

    Add authentication with user model

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['your-frontend-url']
+    origins "*"
 
     resource "*",
       headers: :any,
       expose: %w(Authorization),
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :patch, :put, :delete, :post, :options, :show]
   end
 end
